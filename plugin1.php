391d121f28cee0510cbe3356c444111ed7278ce4
Antonio L <256megas@gmail.com>
2022-08-30 18:53:40 +0200

    add_filter applly_filter

--- plugin1.php
+++ plugin1.php
@@ -30,3 +30,21 @@ add_shortcode( "banner", function($atts, $content){
 	$output = '<div style="background-color: #00A9FF; font-size: 14px; line-height: 24px; color: #070707; text-align: center; padding: 6px 18px;">Suscríbete a mis cursos de tecnologías web <a style="display: inline-block; background-color: #FF7020; border: 1px solid #fff; border-radius: 6px; font-size: 14px; font-weight: normal; color: #fff; padding: 3px 8px; text-decoration: none;" href="#" target="_blank">SUSCRIBIRME</a></div>';
 	return $output;
 });
+
+
+// mine() convierte "rock" en "iron ore"
+// refinery() convierte "iron ore" en "steel"
+function mine($material)
+{
+    return 'iron ore';
+}
+
+function refinery($material){
+    return 'steel';
+}
+
+add_filter('steelmaking', 'mine');
+
+add_filter('steelmaking', 'refinery');
+
+echo apply_filters('steelmaking','rock');
