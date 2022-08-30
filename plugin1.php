720d65ca11f25af8a4eaa3f2d3e575f21c6c1415
Antonio L <256megas@gmail.com>
2022-08-30 12:52:27 +0200

    Shortcut and JS added

--- plugin1.php
+++ plugin1.php
@@ -9,8 +9,24 @@ Author: Antonio
 license: GPL1
 */
 
+// Hooks a los que unir acciones
+// https://adambrown.info/p/wp_hooks/hook
+
+// Mostrar solo en la parte de Administracion (wp-admin)
 if (is_admin()){
-    echo "Soy Administrador";
+    echo "Im Administrator";
 }
 
-echo "Hello World";
+// Añadimos un codigo JavaScript a el Hook footer de la web 
+// 9999 es la posicion en la que se cargara nuestro Script
+add_action('wp_footer', function(){?>
+    <script>
+        console.log("JS loaded on wp_footer")
+    </script>
+    <?php }  ,9999); 
+
+// Crea un ShortCode que se añadira a la página, donde pongamos [banner]
+add_shortcode( "banner", function($atts, $content){
+	$output = '<div style="background-color: #00A9FF; font-size: 14px; line-height: 24px; color: #070707; text-align: center; padding: 6px 18px;">Suscríbete a mis cursos de tecnologías web <a style="display: inline-block; background-color: #FF7020; border: 1px solid #fff; border-radius: 6px; font-size: 14px; font-weight: normal; color: #fff; padding: 3px 8px; text-decoration: none;" href="#" target="_blank">SUSCRIBIRME</a></div>';
+	return $output;
+});
